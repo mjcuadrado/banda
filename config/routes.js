4dd4c72e9ce6e8e50308e8e190ee823ba41a577f
mjcuadrado <mtnweb@gmail.com>
2016-04-20 20:53:50 +0200

    p
    p

--- config/routes.js
+++ config/routes.js
@@ -18,6 +18,7 @@ module.exports = function (app, passport) {
   /**
    * Error handling
    */
+   
 
   app.route('/articulos').get(ArticuloCtrl.dameArticulos);
   app.route('/articulos').post(ArticuloCtrl.nuevo);
