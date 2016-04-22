eed8c66f752942b27346ab8b6f145b3abfc4bf53
mjcuadrado <mtnweb@gmail.com>
2016-04-22 09:36:57 +0200

    fecha3

--- app/controllers/articulos.js
+++ app/controllers/articulos.js
@@ -17,7 +17,7 @@ exports.dameArticulosFecha= function(req, res){
 	var dia=req.params.dia;
 	var mes=req.params.mes;
 	var ano =req.params.ano;
-	var fecha = new date(ano, mes, dia);
+	var fecha = new Date(ano, mes, dia);
 	Articulo.find({"createdAt" : { $gte : fecha }}).exec(function(error, data){
 		if (error) return res.status(500).send(err.message);
 		return res.status(200).jsonp(data);
